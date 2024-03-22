7931d02b6c1ca65f42e0c1ec9177faef3bedb8e0
real-ju <2201181566@qq.com>
2024-03-22 18:24:12 +0800

    路由拦截

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,12 @@
+/**
+ * 路由拦截
+ * 对navigateTo|redirectTo|switchTab|reLaunch四个路由跳转方法进行拦截，检查登录状态
+ * 没有对navigateBack进行拦截，因为系统级返回无法拦截，故这里统一不作处理
+ * 改变登录状态后建议使用reLaunch重启应用
+ * 注意：拦截uni.switchTab本身没有问题。但是在微信小程序端点击tabbar的底层逻辑并不是触发uni.switchTab。所以误认为拦截无效，此类场景的解决方案是在tabbar页面的页面生命周期onShow中处理。
+ * https://uniapp.dcloud.net.cn/api/interceptor.html
+ */
+
 import { store } from '@/store';
 import { routes } from './routes';
 import { BasicPageEnum } from '@/enums/pageEnum';
